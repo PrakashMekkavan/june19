e7c169654b2510a0bdd5e9a2b34d5bf055ce3317
Prakashmekkavan <prakash@gmail.com>
2022-06-21 14:48:00 +0530

    new code

--- src/test/java/March12.java
+++ src/test/java/March12.java
@@ -1,26 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import org.testng.util.RetryAnalyzerCount;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
  
 public class March12 {
  
-	//@Test
-	//private void tc1() {
-//System.out.println("tc1....................");
-//	}
-	@Test( )
-	public void tc2() {
-		Assert.assertTrue(false);
-
-		System.out.println("................tc2...........");
-	}
+	public static void main(String[] args) {
+		
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://www.javatpoint.com/javascript-comment");
+	JavascriptExecutor j= (JavascriptExecutor)driver;
+     WebElement usrname = driver.findElement(By.xpath("//h2[text()='Learn Latest Tutorials']"));
+	j.executeScript("arguments[0].scrollIntoView()",usrname);
 	
-	//@Test
-	//private void tc3() {
-
-		//System.out.println("......tc3................")	;
-}
-//}
-	
+}}
